efb3a631e9c279d33392d0372ac6f4e1ad3b9894
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-28 11:29:22 -0400

    all but remove value are passing

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
@@ -26,7 +29,25 @@ public class ArrayUtility<E extends Object> {
     }
 
     public E getMostCommonFromMerge(E[] arrayToMerge) {
-        return null;
+        E[] mergedArr = mergeArrays(arrayToMerge);
+        E objOne;
+        Integer countOne = 1;
+        E objTwo = null;
+        Integer countTwo = 1;
+        for (int i = 0; i < mergedArr.length-1; i++) {
+            objOne = mergedArr[i];
+            for (int j = i+1; j < mergedArr.length; j++) {
+                if (objOne.equals(mergedArr[j])) {
+                    countOne++;
+                }
+            }
+            if (countOne > countTwo) {
+                objTwo = objOne;
+                countTwo = countOne;
+            }
+            countOne = 1;
+        }
+        return objTwo;
     }
 
     public Integer getNumberOfOccurrences(E valueToEvaluate) {
@@ -40,6 +61,34 @@ public class ArrayUtility<E extends Object> {
     }
 
     public E[] removeValue(E valueToRemove) {
-        return null;
+        int counter = 0;
+        for (E obj : inputArray) {
+            if (obj.equals(valueToRemove)) {
+                counter++;
+            }
+        }
+        ArrayList<E> tempList = new ArrayList<>();
+        for (int i = 0; i < inputArray.length; i++) {
+            if (!inputArray[i].equals(valueToRemove)) {
+                tempList.add(inputArray[i]);
+            }
+        }
+
+        return (E[]) tempList.toArray();
+    }
+
+    public E[] mergeArrays(E[] arrayToMerge) {
+        int fal = inputArray.length;
+        int sal = arrayToMerge.length;
+        Object[] newArr = new Object[fal + sal];
+        for (int i = 0; i < fal; i++) {
+            newArr[i] = inputArray[i];
+        }
+        int newIndex = inputArray.length;
+        for (int j = 0; j < arrayToMerge.length; j++) {
+            newArr[newIndex] = arrayToMerge[j];
+            newIndex++;
+        }
+        return (E[]) newArr;
     }
 }

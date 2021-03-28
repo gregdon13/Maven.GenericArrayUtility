a85b25232ebb79b98e0bebc2d428b98fa99415b0
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-27 21:38:33 -0400

    number of occurrences and count duplicates done

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,43 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<E extends Object> {
+    E[] inputArray;
+
+    public ArrayUtility(E[] inputArray) {
+        this.inputArray = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(E[] arrayToMerge, E valueToEvaluate) {
+        Integer counter = 0;
+        for (int i = 0; i < arrayToMerge.length; i++) {
+            if (arrayToMerge[i] == valueToEvaluate) {
+                counter++;
+            }
+        }
+        for (int j = 0; j < inputArray.length; j++) {
+            if (inputArray[j] == valueToEvaluate) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public E getMostCommonFromMerge(E[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(E valueToEvaluate) {
+        Integer counter = 0;
+        for (E num : inputArray) {
+            if (num == valueToEvaluate) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public E[] removeValue(E valueToRemove) {
+        return null;
+    }
 }
